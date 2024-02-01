2fb35b9774caec6b9711bd0e157be3b02983a611
Cmc0 <dimlt@vip.qq.com>
2024-02-02 00:37:21 +0800

    【i】【优化】【代码】

--- app/src/main/java/com/cmcorg20230301/teamup/MainActivity.java
+++ app/src/main/java/com/cmcorg20230301/teamup/MainActivity.java
@@ -2,6 +2,7 @@ package com.cmcorg20230301.teamup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.cmcorg20230301.teamup.activity.sign.SignInActivity;
 
@@ -11,6 +12,10 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(getColor(R.color.black2));
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         startActivity(new Intent(this, SignInActivity.class));
 
--- app/src/main/java/com/cmcorg20230301/teamup/activity/sign/SignUpActivity.java
+++ app/src/main/java/com/cmcorg20230301/teamup/activity/sign/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.cmcorg20230301.teamup.activity.sign;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.cmcorg20230301.teamup.BaseActivity;
@@ -16,6 +17,10 @@ public class SignUpActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(getColor(R.color.black2));
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         setContentView(R.layout.sign_up);
 
