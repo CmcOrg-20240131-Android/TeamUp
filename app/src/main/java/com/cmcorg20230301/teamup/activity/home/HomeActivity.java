d8405b51abde4075267436d8021a05916e0e5db3
Cmc0 <dimlt@vip.qq.com>
2024-02-27 14:05:07 +0800

    【i】【优化】【代码】

--- app/src/main/java/com/cmcorg20230301/teamup/activity/home/HomeActivity.java
+++ app/src/main/java/com/cmcorg20230301/teamup/activity/home/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.cmcorg20230301.teamup.activity.home;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
 
 import com.cmcorg20230301.teamup.BaseActivity;
 import com.cmcorg20230301.teamup.R;
 import com.cmcorg20230301.teamup.activity.home.chat.HomeChatSessionFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import org.jetbrains.annotations.Nullable;
 
@@ -29,6 +33,33 @@ public class HomeActivity extends BaseActivity {
     public void initView(@Nullable Bundle savedInstanceState) {
 
         setContentView(R.layout.home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemReselectedListener(new NavigationBarView.OnItemReselectedListener() {
+
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.bnm1) {
+
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.homeFragment, HomeChatSessionFragment.class, null)
+                            .commit();
+
+                } else if (itemId == R.id.bnm2) {
+
+
+                } else if (itemId == R.id.bnm3) {
+
+
+                }
+
+            }
+
+        });
 
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.homeFragment, HomeChatSessionFragment.class, null)
