b61385f0053c6b16f16988baf4194edccd9219a3
Cmc0 <dimlt@vip.qq.com>
2024-03-08 10:53:06 +0800

    【i】【优化】【代码】

--- app/src/main/java/com/cmcorg20230301/teamup/MainActivity.java
+++ app/src/main/java/com/cmcorg20230301/teamup/MainActivity.java
@@ -1,14 +1,20 @@
 package com.cmcorg20230301.teamup;
 
 import android.os.Bundle;
+
 import androidx.annotation.ColorRes;
-import cn.hutool.core.util.StrUtil;
+
 import com.cmcorg20230301.teamup.activity.home.HomeActivity;
 import com.cmcorg20230301.teamup.activity.sign.SignActivity;
 import com.cmcorg20230301.teamup.layout.BaseActivity;
 import com.cmcorg20230301.teamup.model.enums.LocalStorageKeyEnum;
 import com.cmcorg20230301.teamup.util.MyLocalStorage;
+import com.cmcorg20230301.teamup.util.common.MyDateUtil;
+
 import org.jetbrains.annotations.Nullable;
+
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,6 +32,22 @@ public class MainActivity extends BaseActivity {
     public void initView(@Nullable Bundle savedInstanceState) {
 
         String jwt = MyLocalStorage.getItem(LocalStorageKeyEnum.JWT);
+
+        String jwtExpireTsStr = MyLocalStorage.getItem(LocalStorageKeyEnum.JWT_EXPIRE_TS);
+
+        Long jwtExpireTs = Convert.toLong(jwtExpireTsStr);
+
+        if (jwtExpireTs != null) {
+
+            long serverTimestamp = MyDateUtil.getServerTimestamp();
+
+            if (jwtExpireTs > serverTimestamp) { // 判断：jwt是否过期
+
+                jwt = null;
+
+            }
+
+        }
 
         if (StrUtil.isBlank(jwt)) {
 
